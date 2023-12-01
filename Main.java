dce92e2374916f4c28b02d7025b969cb9cb32042
ghaliah3074 <148686086+ghaliah3074@users.noreply.github.com>
2023-12-01 11:55:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner e=new Scanner(System.in);
+        
+        //q1 from exercise basic java
+//
+//        System.out.println(" please enter your wight (kg) :  ");
+//        double wight =e.nextDouble();
+//
+//        System.out.println(" please enter your height (m) :");
+//        double height =e.nextDouble();
+//
+//
+//        double BMI= wight*Math.pow(height,2);
+//        System.out.println(" the BMI is "+BMI);
+
+
+
+        //q2
+//
+//        System.out.println(" Enter Obtained Marks: ");
+//        int mark= e.nextInt();
+//
+//        double Percentage=mark%100;
+//        System.out.println("Percentage is: "+Percentage+"%");
+//
+
+
+        //q3 ياخذ مبلغ عملة وسعر الصرف ثم يحولها لعملة اخرى
+
+//
+//        System.out.println("Enter  Amount in USD : ");
+//        double Amount=e.nextDouble();
+//
+//        System.out.println(" Enter Exchange Rate (USD to EUR):  ");
+//        double Exchange_Rate=e.nextDouble();
+//
+//
+//        double res=Amount*Exchange_Rate;
+//        System.out.println("Amount in EUR :"+res);
+
+
+        //q4
+
+//        System.out.println(" enter the sentence ");
+//       StringBuilder sentence;
+//        sentence = new StringBuilder(e.nextLine());
+//        System.out.println("Length of the sentence:"+sentence.length());
+//        System.out.println("Reversed sentence:"+sentence.reverse());
+
+        //q5
+
+//        System.out.println("enter the sentence ");
+//        String sentence2=e.nextLine();
+//
+//        System.out.println(sentence2.substring(10,20));
+//
+
+        //q6
+//        System.out.println(" enter th sentence : ");
+//        String sentence3 =e.nextLine();
+//
+//        System.out.println(" enter the  keyword : ");
+//        String keyword=e.nextLine();
+//
+//        System.out.println(sentence3.concat(keyword));
+
+
+        //q7
+//        System.out.println("enter th sentence :");
+//        String sentence4=e.nextLine();
+//
+//        System.out.println(" enter word the cheng: ");
+//        String chang=e.nextLine();
+//
+//        System.out.println(" enter word the cheng: ");
+//        String chang2=e.nextLine();
+//        System.out.println(sentence4.replace(chang,chang2));
+
+        //q8
+
+//        System.out.println(" enter the word :");
+//        String word1=e.nextLine();
+//        System.out.println(" enter the word :");
+//        String word2=e.nextLine();
+//
+//        System.out.println(word1.equals(word2));// بدون تجاهل الحاله " كبتل سمول"
+//        System.out.println(word1.equalsIgnoreCase(word2)); // مع تجاهل الحاله
+
+
+
+
+
+
+
+
+
+
+    }
+}
